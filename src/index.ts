b384691ee36af57eab5b7df9678497c07fd195fd
Gaurav  Pasarkar <gaurav@technogise.com>
2023-09-16 10:59:02 +0530

    Rename item name to categoryName

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 type Item = {
-  name: string;
+  categoryName: string;
   sellIn: number;
   quality: number;
 };
@@ -14,12 +14,13 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+        this.items[i].categoryName != "Aged Brie" &&
+        this.items[i].categoryName !=
+          "Backstage passes to a TAFKAL80ETC concert"
       ) {
         console.log("Here 1");
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (this.items[i].categoryName != "Sulfuras, Hand of Ragnaros") {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
@@ -27,7 +28,8 @@ export class GildedRose {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+            this.items[i].categoryName ==
+            "Backstage passes to a TAFKAL80ETC concert"
           ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
@@ -42,16 +44,17 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+      if (this.items[i].categoryName != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
+        if (this.items[i].categoryName != "Aged Brie") {
           if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+            this.items[i].categoryName !=
+            "Backstage passes to a TAFKAL80ETC concert"
           ) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (this.items[i].categoryName != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
